35714068a07f2c6b305b1535b5c5ae08b75ca9cf
skulsta <sti.sku@gmail.com>
2022-01-06 17:40:26 +0100

    Adds a few more people, but missing info

--- src/main/kotlin/CharacterList.kt
+++ src/main/kotlin/CharacterList.kt
@@ -1,10 +1,12 @@
 class CharacterList {
     val seperatorLine = "--------------------------------------------------------------------------------"
-    fun instanciatePeople(): List<Character> {
+    fun instanciatePeople(): List<GameCharacter> {
         val people = listOf(
-            Character("Leah", "Cindersap Forest", "Leah's Cottage", listOf("Goat Cheese", "Salad", "Wine")),
-            Character("Emily", "Pelican Town", "2 Willow Lane", listOf("Cloth", "Survival Burger", "Wool")),
-            Character("Penny", "Pelican Town", "Trailer", listOf("Melon", "Poppy", "Roots Platter")),
+            GameCharacter("Leah", "Cindersap Forest", "Leah's Cottage", listOf("Goat Cheese", "Salad", "Wine")),
+            GameCharacter("Emily", "Pelican Town", "2 Willow Lane", listOf("Cloth", "Survival Burger", "Wool")),
+            GameCharacter("Penny", "Pelican Town", "Trailer", listOf("Melon", "Poppy", "Roots Platter")),
+            GameCharacter("Sam", "TBA", "TBA", listOf("TBA")),
+            GameCharacter("Robin", "TBA", "TBA", listOf("TBA")),
         )
         return people
     }
@@ -22,8 +24,8 @@ class CharacterList {
         allPeople.map { println(it.name) }
     }
 
-    fun findCharacter(input: String): List<Character> {
-        val result = mutableListOf<Character>()
+    fun findCharacter(input: String): List<GameCharacter> {
+        val result = mutableListOf<GameCharacter>()
         for (item in allPeople) {
             if (item.name.contains(input, ignoreCase = true)) {
                 result.add(item)
--- src/main/kotlin/GameCharacter.kt
+++ src/main/kotlin/GameCharacter.kt
@@ -1,4 +1,4 @@
-class Character(val name: String, val livesIn: String, val address: String, val bestGifts: List<String>) {
+class GameCharacter(val name: String, val livesIn: String, val address: String, val bestGifts: List<String>) {
     fun printInfo() {
         println("$name lives in $livesIn ($address), and has the following best gifts:")
         for (gift in bestGifts) {
